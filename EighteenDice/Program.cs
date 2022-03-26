0a35feea8aaccb061a8b88f57f82a77d8779b1d2
littletrainee <arechery.a1111@gmail.com>
2022-03-26 19:06:28 +0800

    Add delegate

--- EighteenDice/Program.cs
+++ EighteenDice/Program.cs
@@ -6,13 +6,14 @@
         temp.Add(rnd.Next(1, 7));
       return temp;
     }
+    // delegate
+    public delegate void printhandandpoint();
 
     public static void Main() {
       // instance
       Random rnd = new Random();
       Player p1 = new Player();
       Player p2 = new Player();
-      //WinCheck wc = new WinCheck();
       // set name
       p1.name = "player1";
       p2.name = "player2";
@@ -22,15 +23,17 @@
       // set bookmaker
       p1.bookmaker = true;
 
+      // delegate
+      printhandandpoint pp = new printhandandpoint(p1.PrintHandAndPoint);
+      pp += new printhandandpoint(p2.PrintHandAndPoint);
+
       // player1 roll dice and get point
       p1.hand = RollDice(rnd);
       p1.Check();
       while (p1.point == 0) {
         p1.hand = RollDice(rnd);
         p1.Check();
-        //p1.PrintHandAndPoint();
       }
-      p1.PrintHandAndPoint();
 
       // player2 roll dice and get point
       p2.hand = RollDice(rnd);
@@ -38,10 +41,9 @@
       while (p2.point == 0) {
         p2.hand = RollDice(rnd);
         p2.Check();
-        //p2.PrintHandAndPoint();
       }
-      p2.PrintHandAndPoint();
 
+      pp();
       if (p1.point < p2.point)
         Console.WriteLine("Player2 Win!");
       else if (p1.point > p2.point)
